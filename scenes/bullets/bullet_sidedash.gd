f44f330746e1c7771f7f7da3ee51b507c1b80bbb
Karl Hansson <khansson.nr2@gmail.com>
2021-11-03 23:02:17 +0100

    Add basic dash animation

--- scenes/bullets/bullet_sidedash.gd
+++ scenes/bullets/bullet_sidedash.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Node2D
 
 export(Curve) var target_ease: Curve
 
@@ -10,7 +10,8 @@ enum State {
 	CHARGEDASH
 }
 
-const INITIAL_SPEED = 400
+const INITIAL_SPEED = 150
+const MAX_CHARGE_TIME = 3.0
 
 var simulated_position
 var velocity = Vector2(-INITIAL_SPEED, randf() * -60.0)
@@ -29,31 +30,43 @@ func _physics_process(delta):
 		State.INITIAL:
 			# Simulate a physical position, so that we get a smooth transition out of the hand (claw)
 			velocity.x *= 0.98
-			velocity.y += 50 * delta
+			velocity.y += 100 * delta
 			simulated_position += velocity * delta
 
-			# Calculate the position on the line to follow
-			var pos1 = bullet_line.global_position + bullet_line.points[0]
-			var pos2 = bullet_line.global_position + bullet_line.points[1]
-
-			var global_target_position = lerp(pos1, pos2, sin(time*1.7 + 3.0*randomness) * 0.5 + 0.5)
-			global_target_position.x += cos(time * 2.2) * 4.0
-
-			# Convert the position to coordinates relative to the bullet's space
-			# Which is to say, pretend that we're on the left side.
-			# Right sided bullets are simulated as if left-sided and ultimately mirrored.
-			var target_position = get_parent().get_parent().to_local(global_target_position)
-
-			position = lerp(simulated_position, target_position, target_ease.interpolate(clamp((time*0.5 - 0.2), 0.0, 1.0)))
+			var follow_position = calc_projected_player_position()
+			follow_position.y = lerp(position.y, follow_position.y, 0.1)
+			position = lerp(simulated_position, follow_position, target_ease.interpolate(clamp((time*0.5 - 0.14), 0.0, 1.0)))
 			
-#		State.CHARGEDASH:
+			#position = lerp(position, target_position, 0.01)
+			if time >= MAX_CHARGE_TIME:
+				current_state = State.CHARGEDASH
 			
-#func calc_projected_player_position():
+		State.CHARGEDASH:
+			$AnimationRoot/AnimationPlayer.play("Dash")
+			self.set_physics_process(false)
+			pass
+
+func calc_projected_player_position():
+	# Calculate the position on the line to follow
+	var pos1 = bullet_line.global_position + bullet_line.points[0]
+	var pos2 = bullet_line.global_position + bullet_line.points[1]
+
+	var player_t = (clamp(player.get_global_position().y, pos1.y, pos2.y) - pos1.y) / abs(pos2.y - pos1.y)
+
+#	var global_target_position = lerp(pos1, pos2, sin(time*1.7 + 3.0*randomness) * 0.5 + 0.5)
+	var global_target_position = lerp(pos1, pos2, player_t)
+	global_target_position.x += cos(time * 2.2) * 8.0
+	global_target_position.y += sin(time * 4.6 + randomness * 2.5) * 16.0
+
+	# Convert the position to coordinates relative to the bullet's space
+	# Which is to say, pretend that we're on the left side.
+	# Right sided bullets are simulated as if left-sided and ultimately mirrored.
+	var target_position = to_field_local(global_target_position)
 	
+	return target_position
 	
-	
-	
-	
+func to_field_local(coord: Vector2):
+	return get_parent().get_parent().to_local(coord)
 	
 	
 	
