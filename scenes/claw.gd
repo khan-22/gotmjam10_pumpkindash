ee8f4b40f9863c41e563d44b49d3053627b349bb
Karl Hansson <khansson.nr2@gmail.com>
2021-11-06 16:48:42 +0100

    Rework witch attack system

--- scenes/claw.gd
+++ scenes/claw.gd
@@ -1,9 +1,51 @@
 extends AnimatedSprite
 
 signal bullet_spawn
+signal attack_finished
 
-func trigger_bullet_spawn():
-	emit_signal("bullet_spawn")
+export(float) var animation_speed = 1.0 setget set_animation_speed
+
+var current_idle = "Idle"
 
 func _ready():
+	$ClawAnimationPlayer.playback_speed = animation_speed
+	$ClawAnimationPlayer.connect("animation_finished", self, "return_to_idle")
 	pass
+
+func set_animation_speed(value: float):
+	animation_speed = value
+	$ClawAnimationPlayer.playback_speed = animation_speed
+
+func do_side_attack(bullet_type: PackedScene, bullet_field: Node2D):
+	$ClawAnimationPlayer.play("SideAttack")
+	$ClawAnimationPlayer.queue(current_idle)
+	yield(self, "bullet_spawn")
+	var new_bullet = bullet_type.instance()
+	bullet_field.add_child(new_bullet)
+	new_bullet.set_owner(bullet_field.get_owner())
+	new_bullet.initialize($BulletSpawnPosition.global_position)
+	yield($ClawAnimationPlayer, "animation_changed")
+	emit_signal("attack_finished")
+
+func do_front_attack(bullet_type: PackedScene, bullet_field: Node2D, x_offset: float):
+	var front_anim = $ClawAnimationPlayer.get_animation("FrontAttack")
+	var idx = front_anim.find_track(".:position")
+	var claw_position = front_anim.track_get_key_value(idx, 10) as Vector2
+	claw_position.x = x_offset
+	front_anim.track_set_key_value(idx, 10, claw_position)
+	
+	$ClawAnimationPlayer.play("FrontAttack")
+	$ClawAnimationPlayer.queue(current_idle)
+	yield(self, "bullet_spawn")
+	var new_bullet = bullet_type.instance()
+	bullet_field.add_child(new_bullet)
+	new_bullet.set_owner(bullet_field.get_owner())
+	new_bullet.initialize($BulletSpawnPosition.global_position)
+	yield($ClawAnimationPlayer, "animation_changed")
+	emit_signal("attack_finished")
+
+#func return_to_idle():
+#	print("hello????")
+#	$ClawAnimationPlayer.play(current_idle)
+#	emit_signal("attack_finished")
+
--- scenes/witch.gd
+++ scenes/witch.gd
@@ -1,40 +1,60 @@
 extends Node2D
 
-onready var state_machine = $AnimationTree["parameters/playback"] as AnimationNodeStateMachinePlayback
+#onready var state_machine = $AnimationTree["parameters/playback"] as AnimationNodeStateMachinePlayback
 onready var BulletSideDash = preload("res://scenes/bullets/bullet_sidedash.tscn")
+onready var left_claw = $w_body/w_clawL
+onready var right_claw = $w_body/clawR_pivot/w_clawR
+onready var front_bullet_origin = get_tree().get_nodes_in_group("front_bullet_origin")[0]
 
 var is_firing = false
 
+var current_idle = "Idle"
+
 func _ready():
+#	$BodyAnimationPlayer.connect("animation_finished", self, "return_to_idle")
 	pass
 
-#func _physics_process(delta):
-	
+#func return_to_idle():
+#	print("what?")
+#	$BodyAnimationPlayer.play(current_idle)
 
-
+var i: int = 0
 func _on_FireTimer_timeout():
 	if not is_firing:
-		fire_dual_side_attack()
-	
+		match i % 3:
+			0: 
+				fire_dual_side_attack()
+			1: 
+				fire_left_front_attack()
+			2: 
+				fire_right_front_attack()
+				
+		i += 1
 	pass # Replace with function body.
 
+func fire_left_front_attack():
+	is_firing = true
+	$BodyAnimationPlayer.play("DualSideAttack")
+	$BodyAnimationPlayer.queue(current_idle)
+	left_claw.do_front_attack(BulletSideDash, $BulletManager/LeftSide, -(randi() % 9) * 8)
+#	right_claw.do_side_attack(BulletSideDash, $BulletManager/RightSide)
+	yield(left_claw, "attack_finished")
+	is_firing = false
+
+func fire_right_front_attack():
+	is_firing = true
+	$BodyAnimationPlayer.play("DualSideAttack")
+	$BodyAnimationPlayer.queue(current_idle)
+	right_claw.do_front_attack(BulletSideDash, $BulletManager/RightSide, -(randi() % 9) * 8)
+#	right_claw.do_side_attack(BulletSideDash, $BulletManager/RightSide)
+	yield(right_claw, "attack_finished")
+	is_firing = false
 
 func fire_dual_side_attack():
 	is_firing = true
-	state_machine.travel("DualSideAttack")
-	yield($w_body/w_clawL, "bullet_spawn")
-	var left_bullet = BulletSideDash.instance()
-	var right_bullet = BulletSideDash.instance()
-	
-#	left_bullet.global_position = $w_body/w_clawL/BulletSpawnPosition.global_position
-#	right_bullet.global_position = $w_body/clawR_pivot/w_clawR/BulletSpawnPosition.global_position
-	
-	$BulletManager/LeftSide.add_child(left_bullet)
-	$BulletManager/RightSide.add_child(right_bullet)
-	left_bullet.set_owner(get_owner())
-	right_bullet.set_owner(get_owner())
-	
-	left_bullet.initialize($w_body/w_clawL/BulletSpawnPosition.global_position)
-	right_bullet.initialize($w_body/clawR_pivot/w_clawR/BulletSpawnPosition.global_position)
-	
+	$BodyAnimationPlayer.play("DualSideAttack")
+	$BodyAnimationPlayer.queue(current_idle)
+	left_claw.do_side_attack(BulletSideDash, $BulletManager/LeftSide)
+	right_claw.do_side_attack(BulletSideDash, $BulletManager/RightSide)
+	yield(left_claw, "attack_finished")
 	is_firing = false
